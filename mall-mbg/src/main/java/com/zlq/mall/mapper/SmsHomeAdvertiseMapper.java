c306dcae8b4ec96fab68faa09ef344403f927b09
zhanglianquan <1274096233@qq.com>
2019-07-02 15:51:38 +0800

    首页轮播广告管理

---
+++ mall-mbg/src/main/java/com/zlq/mall/mapper/SmsHomeAdvertiseMapper.java
@@ -0,0 +1,30 @@
+package com.zlq.mall.mapper;
+
+import com.zlq.mall.model.SmsHomeAdvertise;
+import com.zlq.mall.model.SmsHomeAdvertiseExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SmsHomeAdvertiseMapper {
+    int countByExample(SmsHomeAdvertiseExample example);
+
+    int deleteByExample(SmsHomeAdvertiseExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SmsHomeAdvertise record);
+
+    int insertSelective(SmsHomeAdvertise record);
+
+    List<SmsHomeAdvertise> selectByExample(SmsHomeAdvertiseExample example);
+
+    SmsHomeAdvertise selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") SmsHomeAdvertise record, @Param("example") SmsHomeAdvertiseExample example);
+
+    int updateByExample(@Param("record") SmsHomeAdvertise record, @Param("example") SmsHomeAdvertiseExample example);
+
+    int updateByPrimaryKeySelective(SmsHomeAdvertise record);
+
+    int updateByPrimaryKey(SmsHomeAdvertise record);
+}
---
+++ mall-mbg/src/main/java/com/zlq/mall/mapper/SmsHomeNewProductMapper.java
@@ -0,0 +1,30 @@
+package com.zlq.mall.mapper;
+
+import com.zlq.mall.model.SmsHomeNewProduct;
+import com.zlq.mall.model.SmsHomeNewProductExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SmsHomeNewProductMapper {
+    int countByExample(SmsHomeNewProductExample example);
+
+    int deleteByExample(SmsHomeNewProductExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SmsHomeNewProduct record);
+
+    int insertSelective(SmsHomeNewProduct record);
+
+    List<SmsHomeNewProduct> selectByExample(SmsHomeNewProductExample example);
+
+    SmsHomeNewProduct selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") SmsHomeNewProduct record, @Param("example") SmsHomeNewProductExample example);
+
+    int updateByExample(@Param("record") SmsHomeNewProduct record, @Param("example") SmsHomeNewProductExample example);
+
+    int updateByPrimaryKeySelective(SmsHomeNewProduct record);
+
+    int updateByPrimaryKey(SmsHomeNewProduct record);
+}
