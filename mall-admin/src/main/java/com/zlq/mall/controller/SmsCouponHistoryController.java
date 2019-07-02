6633b2496e592bf7ba738e12318878638db13c38
zhanglianquan <1274096233@qq.com>
2019-07-02 15:10:07 +0800

    优惠券领取记录管理

---
+++ mall-admin/src/main/java/com/zlq/mall/controller/SmsCouponHistoryController.java
@@ -0,0 +1,38 @@
+package com.zlq.mall.controller;
+
+
+import com.zlq.mall.dto.CommonResult;
+import com.zlq.mall.model.SmsCouponHistory;
+import com.zlq.mall.service.SmsCouponHistoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * 优惠券领取记录管理Controller
+ */
+@Controller
+@Api(tags = "SmsCouponHistoryController",description = "优惠券领取记录管理")
+@RequestMapping("/couponHistory")
+public class SmsCouponHistoryController {
+    @Autowired
+    private SmsCouponHistoryService historyService;
+    @ApiOperation("根据优惠券id，使用状态，订单编号分页获取领取记录")
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    @ResponseBody
+    public Object list(@RequestParam(value = "couponId",required = false) Long couponId,
+                       @RequestParam(value = "useStatus",required = false) Integer useStatus,
+                       @RequestParam(value = "orderSn",required = false) String orderSn,
+                       @RequestParam(value = "pageSize", defaultValue = "5") Integer pageSize,
+                       @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum){
+        List<SmsCouponHistory> historyList = historyService.list(couponId,useStatus,orderSn,pageSize,pageNum);
+        return new CommonResult().pageSuccess(historyList);
+    }
+}
---
+++ mall-admin/src/main/java/com/zlq/mall/service/impl/SmsCouponHistoryServiceImpl.java
@@ -0,0 +1,37 @@
+package com.zlq.mall.service.impl;
+
+import com.github.pagehelper.PageHelper;
+import com.zlq.mall.mapper.SmsCouponHistoryMapper;
+import com.zlq.mall.model.SmsCouponHistory;
+import com.zlq.mall.model.SmsCouponHistoryExample;
+import com.zlq.mall.service.SmsCouponHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+/**
+ * 优惠券领取记录管理Service实现类
+ */
+@Service
+public class SmsCouponHistoryServiceImpl implements SmsCouponHistoryService {
+    @Autowired
+    private SmsCouponHistoryMapper historyMapper;
+    @Override
+    public List<SmsCouponHistory> list(Long couponId, Integer useStatus, String orderSn, Integer pageSize, Integer pageNum) {
+        PageHelper.startPage(pageNum,pageSize);
+        SmsCouponHistoryExample example = new SmsCouponHistoryExample();
+        SmsCouponHistoryExample.Criteria criteria = example.createCriteria();
+        if(couponId!=null){
+            criteria.andCouponIdEqualTo(couponId);
+        }
+        if(useStatus!=null){
+            criteria.andUseStatusEqualTo(useStatus);
+        }
+        if(!StringUtils.isEmpty(orderSn)){
+            criteria.andOrderSnEqualTo(orderSn);
+        }
+        return historyMapper.selectByExample(example);
+    }
+}
