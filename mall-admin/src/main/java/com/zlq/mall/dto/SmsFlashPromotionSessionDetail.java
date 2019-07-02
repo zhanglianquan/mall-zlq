0132c0806a669033a69cf2523c298b9978bf422b
zhanglianquan <1274096233@qq.com>
2019-07-02 15:33:20 +0800

    限时购场次管理

---
+++ mall-admin/src/main/java/com/zlq/mall/dto/SmsFlashPromotionSessionDetail.java
@@ -0,0 +1,14 @@
+package com.zlq.mall.dto;
+
+import com.zlq.mall.model.SmsFlashPromotionSession;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 包含商品数量的场次信息
+ */
+public class SmsFlashPromotionSessionDetail extends SmsFlashPromotionSession {
+    @Setter
+    @Getter
+    private Integer productCount;
+}
