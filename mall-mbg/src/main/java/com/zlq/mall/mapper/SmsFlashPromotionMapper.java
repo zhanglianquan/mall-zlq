66e00828a6001ea1ffd19251a35520b0e3d9b21d
zhanglianquan <1274096233@qq.com>
2019-07-02 15:20:59 +0800

    限时购活动管理

---
+++ mall-mbg/src/main/java/com/zlq/mall/mapper/SmsFlashPromotionMapper.java
@@ -0,0 +1,30 @@
+package com.zlq.mall.mapper;
+
+import com.zlq.mall.model.SmsFlashPromotion;
+import com.zlq.mall.model.SmsFlashPromotionExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface SmsFlashPromotionMapper {
+    int countByExample(SmsFlashPromotionExample example);
+
+    int deleteByExample(SmsFlashPromotionExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SmsFlashPromotion record);
+
+    int insertSelective(SmsFlashPromotion record);
+
+    List<SmsFlashPromotion> selectByExample(SmsFlashPromotionExample example);
+
+    SmsFlashPromotion selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") SmsFlashPromotion record, @Param("example") SmsFlashPromotionExample example);
+
+    int updateByExample(@Param("record") SmsFlashPromotion record, @Param("example") SmsFlashPromotionExample example);
+
+    int updateByPrimaryKeySelective(SmsFlashPromotion record);
+
+    int updateByPrimaryKey(SmsFlashPromotion record);
+}
