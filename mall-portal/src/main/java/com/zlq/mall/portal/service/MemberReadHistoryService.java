583917664c2b5257771ef880eb90176a4e87a991
zhanglianquan <1274096233@qq.com>
2019-07-16 16:18:55 +0800

    mall-portal -- 会员商品浏览记录管理

---
+++ mall-portal/src/main/java/com/zlq/mall/portal/service/MemberReadHistoryService.java
@@ -0,0 +1,25 @@
+package com.zlq.mall.portal.service;
+
+import com.zlq.mall.portal.domain.MemberReadHistory;
+
+import java.util.List;
+
+/**
+ * 会员浏览记录管理Service
+ */
+public interface MemberReadHistoryService {
+    /**
+     * 生成浏览记录
+     */
+    int create(MemberReadHistory memberReadHistory);
+
+    /**
+     * 批量删除浏览记录
+     */
+    int delete(List<String> ids);
+
+    /**
+     * 获取用户浏览历史记录
+     */
+    List<MemberReadHistory> list(Long memberId);
+}
---
+++ mall-portal/src/main/java/com/zlq/mall/portal/service/impl/MemberReadHistoryServiceImpl.java
@@ -0,0 +1,44 @@
+package com.zlq.mall.portal.service.impl;
+
+import com.zlq.mall.portal.domain.MemberReadHistory;
+import com.zlq.mall.portal.repository.MemberReadHistoryRepository;
+import com.zlq.mall.portal.service.MemberReadHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 会员浏览记录管理Service实现类
+ */
+@Service
+public class MemberReadHistoryServiceImpl implements MemberReadHistoryService {
+    @Autowired
+    private MemberReadHistoryRepository memberReadHistoryRepository;
+    @Override
+    public int create(MemberReadHistory memberReadHistory) {
+        memberReadHistory.setId(null);
+        memberReadHistory.setCreateTime(new Date());
+        memberReadHistoryRepository.save(memberReadHistory);
+        return 1;
+    }
+
+    @Override
+    public int delete(List<String> ids) {
+        List<MemberReadHistory> deleteList = new ArrayList<>();
+        for(String id:ids){
+            MemberReadHistory memberReadHistory = new MemberReadHistory();
+            memberReadHistory.setId(id);
+            deleteList.add(memberReadHistory);
+        }
+        memberReadHistoryRepository.delete(deleteList);
+        return ids.size();
+    }
+
+    @Override
+    public List<MemberReadHistory> list(Long memberId) {
+        return memberReadHistoryRepository.findByMemberIdOrderByCreateTimeDesc(memberId);
+    }
+}
