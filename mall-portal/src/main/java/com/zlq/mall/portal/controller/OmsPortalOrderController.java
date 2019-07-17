58a8e7db64c7bf9ae3b4eaea34524bf2b23b5b0d
zhanglianquan <1274096233@qq.com>
2019-07-18 00:23:22 +0800

    更新

--- mall-portal/src/main/java/com/zlq/mall/portal/controller/OmsPortalOrderController.java
+++ mall-portal/src/main/java/com/zlq/mall/portal/controller/OmsPortalOrderController.java
@@ -13,7 +13,23 @@ import org.springframework.web.bind.annotation.*;
 /**
  * 订单管理Controller
  * 链接：https://juejin.im/post/5cff98986fb9a07ed36ea139
-
+ * 用户名: admin , 密码：123456， {
+ *   "couponId":3,
+ *   "memberReceiveAddressId": 1,
+ *   "payType": 0,
+ *   "useIntegration": 100
+ * }
+ *测试：UmsMemberCouponServiceImpl的listCart函数屏蔽，开启：
+ * //                if(now.before(endTime)&&totalAmount.intValue()>0&&totalAmount.subtract(minPoint).intValue()>=0){
+ * //                    enableList.add(couponHistoryDetail);
+ * //                }else{
+ * //                    disableList.add(couponHistoryDetail);
+ * //                }
+ *
+ *                 enableList.add(couponHistoryDetail);
+ * 每次测试完成优惠券后将表： oms_cart_item的 对应的member_id 的delete_status 重置为0，
+ * sms_coupon_history表对应的member_id 的use_status重置为0，
+ * 这样测试下单成功
  */
 @Controller
 @Api(tags = "OmsPortalOrderController",description = "订单管理")
