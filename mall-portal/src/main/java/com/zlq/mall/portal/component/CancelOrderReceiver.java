5fe8feadd9230da0c57aae4367eeed3953e85a51
zhanglianquan <1274096233@qq.com>
2019-07-18 17:50:57 +0800

    订单取消和rabbitmq 调试ok了， 能够正常的在mq里面看到结果

---
+++ mall-portal/src/main/java/com/zlq/mall/portal/component/CancelOrderReceiver.java
@@ -0,0 +1,26 @@
+package com.zlq.mall.portal.component;
+
+import com.zlq.mall.portal.service.OmsPortalOrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * 取消订单消息的处理者（接收者）
+ */
+@Component
+@RabbitListener(queues = "mall.order.cancel")
+public class CancelOrderReceiver {
+    private static Logger LOGGER =LoggerFactory.getLogger(CancelOrderReceiver.class);
+    @Autowired
+    private OmsPortalOrderService portalOrderService;
+    @RabbitHandler
+    public void handle(Long orderId){
+        portalOrderService.cancelOrder(orderId);
+        LOGGER.info("process orderId:{}",orderId);
+        LOGGER.info("receive delay message orderId:{}",orderId);
+    }
+}
